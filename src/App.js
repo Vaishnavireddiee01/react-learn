9f830a1940a7e197223e8cf49b002d1ce199e1ca
Vaishnavireddiee01 <vanugu47@gmail.com>
2025-04-07 17:45:10 +0530

    styling and css basics

--- src/App.js
+++ src/App.js
@@ -12,10 +12,22 @@ import EventBind from './Component/EventBind'
 import ParentComponent from './Component/ParentComponent'
 import UserGreeting from './Component/UserGreeting';
 import NameList from './Component/NameList';
+import Stylesheet from './Component/Stylesheet';
+import Inline from './Component/Inline';
+import './appStyles.css'
+import styles from './appStyles.module.css'
+
 class App extends Component {
   render() {
     return(
       <div className="App">
+
+        <h1 className='error'>Error</h1>
+        <h1 className={styles.success}>Success</h1>
+
+        {/*using stylesheets*/}
+        <Stylesheet primary={true}/>
+        <Inline/>
         {/*using Lists*/}
         
         <NameList />
