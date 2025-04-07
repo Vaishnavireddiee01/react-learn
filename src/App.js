662bb71856ca0a09c049f5df1b68b198e3c7d05c
Vaishnavireddiee01 <vanugu47@gmail.com>
2025-04-07 15:26:10 +0530

    List rendering

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
     return(
       <div className="App">
         {/*using Lists*/}
+        
         <NameList />
         <UserGreeting/>
         <ParentComponent/>
