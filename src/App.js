cb1daa3fc9b06d9a1b7f6b59e6a90a26d059cea8
Vaishnavireddiee01 <vanugu47@gmail.com>
2025-04-07 18:37:40 +0530

    form handling

--- src/App.js
+++ src/App.js
@@ -16,11 +16,15 @@ import Stylesheet from './Component/Stylesheet';
 import Inline from './Component/Inline';
 import './appStyles.css'
 import styles from './appStyles.module.css'
-
+import './App.css'
+import Form from './Component/Form'
 class App extends Component {
   render() {
     return(
       <div className="App">
+        {/*Forms*/}
+
+        <Form/>
 
         <h1 className='error'>Error</h1>
         <h1 className={styles.success}>Success</h1>
